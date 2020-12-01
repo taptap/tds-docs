f9218a9a3a7206603a3010dcdba6e5d9b420dcc7
caojianbo <caojianbo@taptap.com>
2020-12-01 17:37:58 +0800

    change docusaurus path

--- .ci/build.sh
+++ .ci/build.sh
@@ -4,9 +4,7 @@ rm -f ./build
 SOURCE_VERSION_SHORT="$(git rev-parse --short HEAD)"
 echo $SOURCE_VERSION_SHORT
 ARTIFACT_IMAGE="taptap-img-registry-vpc.cn-beijing.cr.aliyuncs.com/tds/tapsdkdoc:${SOURCE_VERSION_SHORT}"
-yarn build
-
-pwd;
-ls;
-# docker build -t $ARTIFACT_IMAGE . 
-# docker push $ARTIFACT_IMAGE           
+# yarn build
+node_modules/.bin/docusaurus build
+docker build -t $ARTIFACT_IMAGE . 
+docker push $ARTIFACT_IMAGE           
