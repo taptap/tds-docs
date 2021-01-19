7b4b7414537f370ab5abe3d5fd9aeea0dba9a83d
ci-gitlab <caojianbo@xd.com>
2021-01-19 16:42:13 +0800

    revert base url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -4,7 +4,7 @@
 module.exports = {
   title: 'TapTap 开发者文档中心',
   url: 'https://your-docusaurus-test-site.com', // todo should overwrite
-  baseUrl: '/v2-doc/',
+  baseUrl: '',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: '/img/favicon.ico',
