161784796c449e8240e1684369032085260a9196
付业成 <fuyecheng@xindong.com>
2021-01-19 16:17:37 +0800

    fix bug

--- .ci/build.sh
+++ .ci/build.sh
@@ -27,7 +27,7 @@ ARTIFACT_IMAGE="taptap-img-registry-vpc.cn-beijing.cr.aliyuncs.com/tds/tapsdkdoc
 ret=0
 node_modules/.bin/docusaurus build || ret=$?
 
-if [ req -eq 0 ]; then 
+if [ $ret -eq 0 ]; then 
   docker build -t $ARTIFACT_IMAGE . 
   docker push $ARTIFACT_IMAGE        
   docker rmi ${ARTIFACT_IMAGE}   
