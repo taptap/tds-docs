f18ef58e29b150bbfca0d9df1e42a461b2d07f97
JemyCheung <251839954@qq.com>
2021-04-21 14:08:54 +0800

    add dlc & alive

--- sidebars.js
+++ sidebars.js
@@ -19,8 +19,11 @@ module.exports = {
       功能介绍: ['sdk/tap-fun-login',
         'sdk/tap-fun-db',
         'sdk/tap-fun-moment',
+        'sdk/tap-fun-usertest',
         'sdk/tap-fun-paysdk',
-        'sdk/tap-fun-usertest'],
+        'tap-fun-alive',
+        'tap-fun-dlc'
+      ],
     },
     {
       API: [
