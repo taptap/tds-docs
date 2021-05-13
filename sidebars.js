f988d05a6cb84a6bf3debd8ce94745f376353a5f
JemyCheung <251839954@qq.com>
2021-05-13 17:41:22 +0800

    add pay sdk

--- sidebars.js
+++ sidebars.js
@@ -14,7 +14,8 @@ module.exports = {
         'sdk/tap-fun-db',
         'sdk/tap-fun-moment',
         'sdk/tap-fun-usertest',
-        'sdk/tap-fun-paysdk'
+        'sdk/tap-fun-paysdk',
+        'sdk/tap-fun-update'
       ],
     },
     {
