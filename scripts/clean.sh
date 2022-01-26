e8621c45679385201d310da66c765453891efd7c
Fuchen Shi <shifuchen@xd.com>
2022-01-26 11:26:52 +0800

    build: update CI configs

--- scripts/clean.sh
+++
@@ -1,4 +0,0 @@
-rm -rf content
-rm -rf static docs src/docComponents
-rm -rf i18n/en/docusaurus-plugin-content-docs && mkdir -p i18n/en/docusaurus-plugin-content-docs
-rm -rf i18n/eng/docusaurus-plugin-content-docs && mkdir -p i18n/eng/docusaurus-plugin-content-docs
