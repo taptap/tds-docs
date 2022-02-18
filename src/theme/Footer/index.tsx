f7c5a99f4a9278db4ce08715440536e527ba21ff
Fuchen Shi <91254072+fuchenshi@users.noreply.github.com>
2022-02-18 09:53:00 +0800

    refactor(footer) (#35)
    * refactor(footer)
    
    * chore: remove `luxon`
    
    * fix(footer)

--- src/theme/Footer/index.tsx
+++ src/theme/Footer/index.tsx
@@ -1,106 +1,110 @@
 import React from "react";
-import { DateTime } from "luxon";
 import Link from "@docusaurus/Link";
 import Translate from "@docusaurus/Translate";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import styles from "./styles.module.scss";
 import Logo from "@theme/Logo";
-import { externalLinkList, toInnerLinkList } from "./_config";
-import { REGION } from "../../constants/env";
+import { BRAND, REGION } from "../../constants/env";
+import { getNavLinks, getLicenceLinks } from "./_config";
 
-function copyRightNotice(region) {
-  // taptap.com is operated by YiWan (Shanghai), and taptap.io is operated by TapTap Inc (in singapore).
-  // 'Asia/Shanghai' and 'Asia/Singapore' are both UTC+8 currently, but this might change in future.
-  const timezone = region === "cn" ? "Asia/Shanghai" : "Asia/Singapore";
-  const currentYear = DateTime.now().setZone(timezone).year;
-  const year = currentYear > 2021 ? `2021–${currentYear}` : "2021";
-  return `©${year} TapTap`;
+function getCopyrightNotice(brand: string): string {
+  const year = new Date().getFullYear();
+  const company =
+    brand === "leancloud" ? "美味书签（北京）信息技术有限公司" : "TapTap";
+  return `©${year} ${company}`;
 }
 
 function Footer() {
   const {
-    i18n: { currentLocale, defaultLocale },
+    i18n: { currentLocale },
   } = useDocusaurusContext();
-  const isDefaultLocale = currentLocale === defaultLocale;
-  const localePath = isDefaultLocale ? "" : `${currentLocale}/`;
 
   return (
-    <footer className={styles.footerContainer}>
+    <footer className={styles.footer}>
       <div className={styles.footerContent}>
-        <div className={styles.logoRow}>
-          {/* @ts-ignore */}
-          <Logo noLabel />
-        </div>
-        <div className={styles.linkRow}>
-          {toInnerLinkList(localePath, REGION).map((item) => {
-            return item.link ? (
-              <Link
-                key={item.label + item.link}
-                className={styles.linkItem}
-                to={item.link}
-              >
-                {item.label}
-              </Link>
-            ) : (
-              <a
-                key={item.label + item.url}
-                className={styles.linkItem}
-                href={item.url}
-                rel="noreferrer nofollow noopener"
-                target="_blank"
-              >
-                {item.label}
-              </a>
-            );
-          })}
-        </div>
-        <div className={styles.infoRow}>
-          {REGION === "cn" ? (
-            <>
-              <div className={styles.externalItem}>
-                <Translate
-                  id="tds-footer-易玩（上海）网络科技有限公司"
-                  description="from Footer"
+        <div>
+          <section className={styles.logo}>
+            {/* @ts-ignore */}
+            <Logo noLabel />
+          </section>
+
+          <section className={styles.nav}>
+            {getNavLinks(BRAND, REGION, currentLocale).map((item) =>
+              "link" in item ? (
+                <Link
+                  to={item.link}
+                  className={styles.navItem}
+                  key={item.label}
                 >
-                  易玩（上海）网络科技有限公司
-                </Translate>
-              </div>
-              <div className={styles.externalItem}>
-                <Translate
-                  id="tds-footer-公司地址: 上海市静安区万荣路 700 号 A3 202"
-                  description="from Footer"
+                  {item.label}
+                </Link>
+              ) : (
+                <a
+                  href={item.url}
+                  target="_blank"
+                  rel="noreferrer nofollow noopener"
+                  className={styles.navItem}
+                  key={item.label}
                 >
-                  公司地址: 上海市静安区万荣路 700 号 A3 202
-                </Translate>
-              </div>
-              <div className={styles.externalItem}>
-                <Translate
-                  id="tds-footer-注册地址: 上海市闵行区紫星路 588 号 2 幢 2122 室"
-                  description="from Footer"
-                >
-                  注册地址: 上海市闵行区紫星路 588 号 2 幢 2122 室
-                </Translate>
-              </div>
-            </>
-          ) : (
-            <div className={styles.externalItem}>TapTap Pte. Ltd.</div>
-          )}
-          <div className={styles.externalItem}>{copyRightNotice(REGION)}</div>
+                  {item.label}
+                </a>
+              )
+            )}
+          </section>
+
+          <section className={styles.info}>
+            {BRAND === "tds" &&
+              (REGION === "cn" ? (
+                <>
+                  <div>
+                    <Translate
+                      id="tds-footer-易玩（上海）网络科技有限公司"
+                      description="from Footer"
+                    >
+                      易玩（上海）网络科技有限公司
+                    </Translate>
+                  </div>
+                  <div>
+                    <Translate
+                      id="tds-footer-公司地址: 上海市静安区万荣路 700 号 A3 202"
+                      description="from Footer"
+                    >
+                      公司地址: 上海市静安区万荣路 700 号 A3 202
+                    </Translate>
+                  </div>
+                  <div>
+                    <Translate
+                      id="tds-footer-注册地址: 上海市闵行区紫星路 588 号 2 幢 2122 室"
+                      description="from Footer"
+                    >
+                      注册地址: 上海市闵行区紫星路 588 号 2 幢 2122 室
+                    </Translate>
+                  </div>
+                </>
+              ) : (
+                <div>TapTap Pte. Ltd.</div>
+              ))}
+
+            <div>{getCopyrightNotice(BRAND)}</div>
+          </section>
         </div>
-        {REGION === "cn" && isDefaultLocale && (
-          <div className={styles.recordRow}>
-            {externalLinkList.map((item) => (
-              <a
-                key={item.label + item.link}
-                className={styles.externalItem}
-                href={item.link}
-                rel="noreferrer nofollow noopener"
-                target="_blank"
-              >
-                {item.icon && <img src={item.icon} alt={item.label} />}
-                {item.label}
-              </a>
-            ))}
+
+        {getLicenceLinks(BRAND, REGION, currentLocale).length > 0 && (
+          <div>
+            <section className={styles.licence}>
+              {getLicenceLinks(BRAND, REGION, currentLocale).map((item) => (
+                <a
+                  href={item.link}
+                  target="_blank"
+                  rel="noreferrer nofollow noopener"
+                  className={styles.licenceItem}
+                  key={item.label}
+                >
+                  {item.icon && <img src={item.icon} alt={item.label} />}
+                  {item.label}
+                </a>
+              ))}
+            </section>
           </div>
         )}
       </div>
--- src/theme/Logo/index.tsx
+++ src/theme/Logo/index.tsx
@@ -1,29 +1,49 @@
-import React from 'react';
-import useBaseUrl from '@docusaurus/useBaseUrl';
-import styles from './styles.module.scss';
-import Translate from '@docusaurus/Translate';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import React from "react";
+import useBaseUrl from "@docusaurus/useBaseUrl";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import Translate from "@docusaurus/Translate";
+import { BRAND } from "../../constants/env";
+import styles from "./styles.module.scss";
 
-type LogoProp = {
+interface LogoProps {
   noLabel?: boolean;
-  onClick?: () => any;
-};
+}
 
-const Logo = ({ noLabel, onClick }: LogoProp) => {
+const Logo = ({ noLabel }: LogoProps) => {
   const { siteConfig } = useDocusaurusContext();
-  const mainDomainHost = (siteConfig.customFields?.mainDomainHost ?? '') as string;
-  const dcDomainHost = (siteConfig.customFields?.dcDomainHost ?? '') as string;
-  return <div className={styles.logoContainer} onClick={onClick}>
-    <a className={styles.logoImage} href={mainDomainHost}>
-      <img src={useBaseUrl('img/logo.svg')} alt="TapTap" />
-    </a>
-    {!noLabel && <div className={styles.divider} />}
-    {!noLabel && <a className={styles.label} href={dcDomainHost}>
-      <Translate id="tds-header-开发者中心" description="from Header">
-        开发者中心
-      </Translate>
-    </a>}
-  </div>;
+  const mainDomainHost = (siteConfig.customFields?.mainDomainHost ??
+    "") as string;
+  const dcDomainHost = (siteConfig.customFields?.dcDomainHost ?? "") as string;
+
+  return (
+    <div className={styles.logoContainer}>
+      <a className={styles.logoImage} href={mainDomainHost}>
+        <img
+          src={
+            BRAND === "leancloud"
+              ? useBaseUrl("img/leancloud.svg")
+              : useBaseUrl("img/logo.svg")
+          }
+          alt={BRAND === "leancloud" ? "LeanCloud" : "TapTap"}
+          style={{
+            height: BRAND === "leancloud" ? 20 : 24,
+            transform: BRAND === "leancloud" ? "none" : "translateY(1px)",
+          }}
+        />
+      </a>
+
+      {BRAND === "tds" && !noLabel && (
+        <>
+          <div className={styles.divider} />
+          <a className={styles.label} href={dcDomainHost}>
+            <Translate id="tds-header-开发者中心" description="from Header">
+              开发者中心
+            </Translate>
+          </a>
+        </>
+      )}
+    </div>
+  );
 };
 
 export default Logo;
