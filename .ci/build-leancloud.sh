fa198cf8ca3ff4b0037923034048cd980b473b92
Fuchen Shi <shifuchen@xd.com>
2022-02-18 17:43:31 +0800

    feat: add color palette for LeanCloud docs

--- .ci/build-leancloud.sh
+++ .ci/build-leancloud.sh
@@ -4,8 +4,9 @@ set -e
 
 mkdir tmp
 cp -r docs/sdk/_partials docs/sdk/16-engine tmp
-rm -rf docs i18n src/constants/env.ts versioned_docs versioned_sidebars docusaurus.config.js sidebars.js versions.json
+rm -rf docs i18n src/constants/env.ts src/styles/override.scss versioned_docs versioned_sidebars docusaurus.config.js sidebars.js versions.json
 cp .ci/leancloud/env.ts src/constants/env.ts
+cp .ci/leancloud/override.scss src/styles/override.scss
 cp .ci/leancloud/docusaurus.config.js docusaurus.config.js
 cp .ci/leancloud/sidebars.js sidebars.js
 mkdir docs
