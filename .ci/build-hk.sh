e87d9b69bb2ac62350258c6effaf17ee4a07f4df
Fuchen Shi <shifuchen@xd.com>
2022-02-22 17:30:27 +0800

    chore: update project structure

--- .ci/build-hk.sh
+++ .ci/build-hk.sh
@@ -2,12 +2,11 @@
 
 set -e
 
-rm -rf docs i18n/en src/constants/env.ts versioned_docs versioned_sidebars docusaurus.config.js sidebars.js versions.json
+rm -rf docs i18n src/constants/env.ts versioned_docs versioned_sidebars docusaurus.config.js sidebars.js versions.json
 cp .ci/hk/env.ts src/constants/env.ts
 cp .ci/hk/docusaurus.config.js docusaurus.config.js
 cp .ci/hk/sidebars.js sidebars.js
-mv i18n/eng i18n/en
-mv i18n/zh-Hansg i18n/zh-Hans
+mv i18n-global i18n
 cp -r i18n/en/docusaurus-plugin-content-docs/current docs
 
 yarn build --out-dir build-hk
