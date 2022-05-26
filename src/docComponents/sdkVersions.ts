9bb3f25d58ee71e39e543fc9925c12b467ddfad7
kylin <kylin37091@gmail.com>
2022-05-26 10:39:27 +0800

    chore: sdk version bump

--- src/docComponents/sdkVersions.ts
+++ src/docComponents/sdkVersions.ts
@@ -1,8 +1,8 @@
 const sdkVersions = {
   taptap: {
-    unity: "3.8.0",
-    android: "3.8.0",
-    ios: "3.8.0",
+    unity: "3.9.0",
+    android: "3.9.0",
+    ios: "3.9.0",
     rtc: "1.1.0",
   },
   tapdb: {
@@ -11,14 +11,14 @@ const sdkVersions = {
     unity: "3.1.3",
   },
   leancloud: {
-    objc: "13.6.1",
+    objc: "13.9.0",
     swift: "17.10.1",
     js: {
-      storage: "4.12.0",
+      storage: "4.12.2",
       realtime: "5.0.0-rc.7",
     },
-    java: "8.2.8",
-    csharp: "0.10.10",
+    java: "8.2.9",
+    csharp: "0.10.11",
   },
 };
 
