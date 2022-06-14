7a362f6ae927271b36100e62b9982c7c864ec136
Fuchen Shi <shifuchen@xd.com>
2022-06-14 16:57:47 +0800

    feat: add SDK setup guides for LeanCloud

--- .ci/build-leancloud.sh
+++ .ci/build-leancloud.sh
@@ -3,7 +3,10 @@
 set -e
 
 mkdir tmp
-cp -r docs/sdk/_partials docs/sdk/domain docs/sdk/engine tmp
+cp -r docs/sdk/_partials docs/sdk/domain docs/sdk/engine docs/sdk/storage/_partials tmp
+cp -r docs/sdk/storage/dotnet-guide/_category_.json docs/sdk/storage/dotnet-guide/setup-dotnet.mdx tmp
+cp -r docs/sdk/storage/java-guide/_category_.json docs/sdk/storage/java-guide/setup-java.mdx tmp
+cp -r docs/sdk/storage/objc-guide/_category_.json docs/sdk/storage/objc-guide/setup-objc.mdx tmp
 rm -rf docs i18n src/constants/env.ts src/styles/override.scss versioned_docs versioned_sidebars docusaurus.config.js sidebars.js versions.json
 cp .ci/leancloud/env.ts src/constants/env.ts
 cp .ci/leancloud/override.scss src/styles/override.scss
