6ae7abe1667a6b940ebffbfcceb5ed9f447c7f5a
Fuchen Shi <shifuchen@xd.com>
2022-06-14 17:47:32 +0800

    fix: error in build script

--- .ci/build-leancloud.sh
+++ .ci/build-leancloud.sh
@@ -3,10 +3,8 @@
 set -e
 
 mkdir tmp
-cp -r docs/sdk/_partials docs/sdk/domain docs/sdk/engine docs/sdk/storage/_partials tmp
-cp -r docs/sdk/storage/dotnet-guide/_category_.json docs/sdk/storage/dotnet-guide/setup-dotnet.mdx tmp
-cp -r docs/sdk/storage/java-guide/_category_.json docs/sdk/storage/java-guide/setup-java.mdx tmp
-cp -r docs/sdk/storage/objc-guide/_category_.json docs/sdk/storage/objc-guide/setup-objc.mdx tmp
+cp -r docs/sdk/_partials docs/sdk/domain docs/sdk/engine docs/sdk/storage tmp
+rm -rf tmp/storage/dotnet-guide/dotnet.mdx tmp/storage/fulltext-search tmp/storage/java-guide/java.mdx tmp/storage/objc-guide/objc.mdx tmp/storage/features.mdx tmp/storage/rest.mdx tmp/storage/security.mdx
 rm -rf docs i18n src/constants/env.ts src/styles/override.scss versioned_docs versioned_sidebars docusaurus.config.js sidebars.js versions.json
 cp .ci/leancloud/env.ts src/constants/env.ts
 cp .ci/leancloud/override.scss src/styles/override.scss
