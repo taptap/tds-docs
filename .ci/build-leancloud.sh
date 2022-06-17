50727cb2b04cebd75edf0536b64b98cb2508d3a3
Fuchen Shi <shifuchen@xd.com>
2022-06-17 16:13:01 +0800

    feat(LeanCloud): add Data Security

--- .ci/build-leancloud.sh
+++ .ci/build-leancloud.sh
@@ -5,8 +5,8 @@ set -e
 mkdir tmp tmp/zh-Hans tmp/en
 cp -r docs/sdk/_partials docs/sdk/domain docs/sdk/engine docs/sdk/storage tmp/zh-Hans
 cp -r i18n/en/docusaurus-plugin-content-docs/current/sdk/_partials i18n/en/docusaurus-plugin-content-docs/current/sdk/storage tmp/en
-rm -rf tmp/zh-Hans/storage/fulltext-search tmp/zh-Hans/storage/features.mdx tmp/zh-Hans/storage/rest.mdx tmp/zh-Hans/storage/security.mdx
-rm -rf tmp/en/storage/fulltext-search tmp/en/storage/features.mdx tmp/en/storage/rest.mdx tmp/en/storage/security.mdx
+rm -rf tmp/zh-Hans/storage/fulltext-search tmp/zh-Hans/storage/features.mdx tmp/zh-Hans/storage/rest.mdx
+rm -rf tmp/en/storage/fulltext-search tmp/en/storage/features.mdx tmp/en/storage/rest.mdx
 rm -rf docs i18n/en/docusaurus-plugin-content-docs/current src/constants/env.ts src/styles/override.scss versioned_docs versioned_sidebars docusaurus.config.js sidebars.js versions.json
 cp .ci/leancloud/env.ts src/constants/env.ts
 cp .ci/leancloud/override.scss src/styles/override.scss
--- src/docComponents/path.tsx
+++ src/docComponents/path.tsx
@@ -7,20 +7,33 @@ const PRODUCT_NAMES: {
   tds: {
     "zh-Hans": {
       storage: "数据存储",
+      auth: "内建账户",
       engine: "云引擎",
       im: "即时通讯",
+      push: "推送通知",
     },
     en: {
       storage: "Data Storage",
+      auth: "TDS Authentication",
       engine: "Cloud Engine",
       im: "Instant Messaging",
+      push: "Push Notification",
     },
   },
   leancloud: {
     "zh-Hans": {
       storage: "数据存储",
+      auth: "内建账户",
       engine: "云引擎",
       im: "即时通讯",
+      push: "推送",
+    },
+    en: {
+      storage: "Data Storage",
+      auth: "Authentication",
+      engine: "LeanEngine",
+      im: "Instant Messaging",
+      push: "Push Notification",
     },
   },
 };
