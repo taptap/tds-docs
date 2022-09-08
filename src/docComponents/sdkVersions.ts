27d212355c176d77d5ee2052c20dd1f25c1d5c8f
Fuchen Shi <shifuchen@xd.com>
2022-09-08 11:16:31 +0800

    chore: bump SDK versions

--- src/docComponents/sdkVersions.ts
+++ src/docComponents/sdkVersions.ts
@@ -1,19 +1,19 @@
 const sdkVersions = {
   taptap: {
-    unity: "3.13.0",
-    android: "3.13.0",
-    ios: "3.13.0",
+    unity: "3.14.0",
+    android: "3.14.0",
+    ios: "3.14.0",
     rtc: "1.1.0",
   },
   leancloud: {
     objc: "13.9.0",
     swift: "17.10.1",
     js: {
-      storage: "4.13.1",
+      storage: "4.13.2",
       realtime: "5.0.0-rc.7",
     },
     java: "8.2.12",
-    csharp: "0.10.14",
+    csharp: "0.10.17",
   },
 };
 
