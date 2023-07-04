526461af559a7aa3bccf07bf0750282a541e7593
ShouWang <514504765@qq.com>
2023-07-04 09:29:28 +0800

    docs: update TapPayments for io (#595)
    * docs: tappayments for en
    
    * docs: delete redundant descriptions
    
    * docs: update pic for io
    
    * docs: update sdk version
    
    * Update tds-tappay-commodity-management.mdx
    
    * Update tds-tappay-introduce.mdx
    
    * Update tds-tappay-orders-refunds.mdx
    
    * Update tds-tappay-product-description.mdx
    
    ---------
    
    Co-authored-by: Fuchen Shi <91254072+fuchenshi@users.noreply.github.com>

--- src/docComponents/sdkVersions.ts
+++ src/docComponents/sdkVersions.ts
@@ -1,8 +1,8 @@
 const sdkVersions = {
   taptap: {
-    unity: "3.18.7",
-    android: "3.18.7",
-    ios: "3.18.7",
+    unity: "3.18.8",
+    android: "3.18.8",
+    ios: "3.18.8",
     rtc: "1.1.0",
   },
   leancloud: {
