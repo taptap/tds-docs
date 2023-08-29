4ba4525ac1ea3b2a418854397934911a9f36f1de
hzpeng57 <hrrmzp@163.com>
2023-08-29 14:18:25 +0800

    feat: change Footer

---
+++ src/theme/Footer/TdsFooter/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import { Footer as SFooter, Language } from './lib/ssr/footer'
+import './lib/ssr/style.css';
+
+function TdsFooter() {
+  const {
+    i18n: { currentLocale },
+  } = useDocusaurusContext();
+  const { html } = SFooter.render({
+    language: currentLocale === 'en' ? Language.en : Language.zh_CN,
+    tapUrl: 'https://www.taptap.cn',
+  })
+  return (<div dangerouslySetInnerHTML={{__html: html}}></div>);
+};
+
+export default TdsFooter;
