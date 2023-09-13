a14b7ae4bd04cfab6a22f65c1db00d9982ea9dd3
yuwenjian <740225978@qq.com>
2023-09-13 10:25:17 +0800

    更新ReleasesNote组件

--- src/theme/ReleaseNote/index.tsx
+++ src/theme/ReleaseNote/index.tsx
@@ -1,49 +1,37 @@
-import React, { useState, useEffect, useRef } from "react"
+import React, { useState, useEffect } from "react"
 import ReactMarkdown from 'react-markdown';
 
+import { findReleaseContent } from "./api";
 
 interface UnityReleaseProps {
   sdkPlugName: string;
   sdkName: string;
 }
 
-
-
 const UnityRelease = ({
   sdkPlugName,
   sdkName
 }: UnityReleaseProps) => {
 
-
-    const AV = require("leancloud-storage");
-    const { Query, User } = AV;
-
-
-    AV.init({
-        appId: "lhzo7z96ayhad9flpynyiu79t2jpzuasz2ke8cdb09zduvug",
-        appKey: "0k0wpd4ee07yoi9ecrltd2lqjjna6njnkp7u0mfhd8e1ft3h",
-        serverURL: "https://lhzo7z96.lc-cn-n1-shared.com",
-      });
-
     const [content, setContent] = useState(undefined)
 
+
     useEffect( () => {
-        onSearch()
+      onSearch()
     }, [])
 
-    
-    // 获取列表数据
-    const onSearch = () => {
+    const onSearch = async () => {
 
-      const query = new AV.Query("Release");
-      query.equalTo("sdkPlugName", sdkPlugName);
-      query.equalTo("sdkName", sdkName);
+      try {
+        const result = await findReleaseContent(sdkPlugName, sdkName)
+        const contentInfo = result.data._attributes.content
+        setContent(contentInfo);
+      } catch (error) {
+        console.error(error);
+      }
+   }
 
-      query.find().then((contentInfo) => {
-        setContent(contentInfo[0]["attributes"]["content"]);
-      });
-    }
-
+  
     return (
       <div>
         <ReactMarkdown children={content} />
@@ -52,3 +40,4 @@ const UnityRelease = ({
   };
   
 export default UnityRelease;
+
