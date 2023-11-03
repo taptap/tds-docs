8d5ec676c0bdb6309e5d75896de8a5b49656d385
harryphone <harryphone@163.com>
2023-11-03 10:42:01 +0800

    feat: 修改版本号

--- src/docComponents/sdkVersions.ts
+++ src/docComponents/sdkVersions.ts
@@ -1,8 +1,9 @@
 const sdkVersions = {
   taptap: {
-    unity: "3.25.0",
-    android: "3.23.0",
-    ios: "3.22.0",
+    unity: "3.26.0",
+    android: "3.24.0",
+    ios: "3.23.0",
+    unreal: "3.25.0",
     rtc: "1.1.0",
     adr: "1.1.5"
   },
