0e9e4a6874f611d904a4e330a1532692f18f7959
xd <caozhenrui@xd.com>
2023-12-19 10:24:04 +0800

    fix 头标修改

--- src/theme/Logo/index.tsx
+++ src/theme/Logo/index.tsx
@@ -72,10 +72,10 @@ const Logo = ({ noLabel, noLink, reversed, size = 1 }: LogoProps) => {
         {BRAND === "leancloud" ? LOGOS.leancloud : LOGOS.tds}
       </div> :  
        (currentLocale === "en")?  <div>
-       <img src={useBaseUrl("/img/logo_en.png")} width="380" height="30"></img>
+       <img src={useBaseUrl("/img/logo_top_en.png")} width="218" height="30"></img>
       </div>:
         <div>
-        <img src={useBaseUrl("/img/Local.png")} width="218" height="30"></img>
+        <img src={useBaseUrl("/img/logo_top_cn.png")} width="218" height="30"></img>
 
       </div>
       }
