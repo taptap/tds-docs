ce742a3e24c8a47dccd18a6bde5d0e604026f8bd
lichao <lichao@taptap.com>
2023-12-21 19:51:27 +0800

    update antiAddiction init interface in 3.27.0

--- src/docComponents/sdkVersions.ts
+++ src/docComponents/sdkVersions.ts
@@ -1,7 +1,7 @@
-const taptapUnity = "3.26.5"
-const taptapIos = "3.23.1"
-const taptapAndroid = "3.24.1"
-const taptapUnreal = "3.25.1"
+const taptapUnity = "3.27.0"
+const taptapIos = "3.27.0"
+const taptapAndroid = "3.27.0"
+const taptapUnreal = "3.27.0"
 
 const sdkVersions = {
   taptap: {
