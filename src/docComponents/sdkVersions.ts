7e7f495a661cf64da51065cb959dcca75e876866
Li Ye <liye@xd.com>
2024-04-12 11:44:22 +0800

    feat: LeanCloud SDK 下载页展示版本号
    并将版本号更新到了最新

--- src/docComponents/sdkVersions.ts
+++ src/docComponents/sdkVersions.ts
@@ -13,14 +13,14 @@ const sdkVersions = {
     adr: "1.2.2"
   },
   leancloud: {
-    objc: "13.9.0",
-    swift: "17.10.1",
+    objc: "14.0.0",
+    swift: "17.11.0",
     js: {
-      storage: "4.13.2",
-      realtime: "5.0.0-rc.7",
+      storage: "4.15.2",
+      realtime: "5.0.0-rc.8",
     },
     java: "8.2.24",
-    csharp: "2.3.0",
+    csharp: "2.4.1",
     flutter: {
       storage: "0.7.10",
       realtime: "1.0.1",
