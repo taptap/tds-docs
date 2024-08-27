065879321f5523cf60123af7f257a1ad90cfb4b9
huangliling <huangliling@xd.com>
2024-08-27 18:29:11 +0800

    refactor: leancloud 文档目录结构

--- .ci/build-leancloud.sh
+++ .ci/build-leancloud.sh
@@ -3,17 +3,17 @@
 set -e
 
 # 移除 TDS 文档
-rm -rf docs i18n tds_hk versioned_docs versioned_sidebars versions.json
+rm -rf docs i18n versioned_docs versioned_sidebars versions.json
+
 # 移动 LC 文档
 mv leancloud/docs .
 mv leancloud/i18n .
+
 # 替换配置文件
 mv leancloud/conf/env.ts src/constants/env.ts
 cp leancloud/conf/override.scss src/styles/override.scss
 cp leancloud/conf/docusaurus.config.js docusaurus.config.js
 cp leancloud/conf/sidebars.js sidebars.js
-# 删除 leancloud 目录
-rm -rf leancloud
 
 # 构建
 yarn build --out-dir build-leancloud
