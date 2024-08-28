4a3361b7cf99d4212c0a8f88b58f98f7d296c380
zhaoyang <zhaoyang1@xd.com>
2024-08-28 17:19:10 +0800

    refactor: docs format

--- versioned_docs/version-v4/sdkVersions.ts
+++ versioned_docs/version-v4/sdkVersions.ts
@@ -1,6 +1,6 @@
-const taptapUnity = "4.3.8"
-const taptapIos = "4.3.8"
-const taptapAndroid = "4.3.8"
+const taptapUnity = "4.3.10"
+const taptapIos = "4.3.10"
+const taptapAndroid = "4.3.10"
 const taptapUnreal = "3.29.2"
 
 const sdkVersions = {
