29bbacfff2c5e406d994c5086811a0b83d7d974d
huangliling <huangliling@xd.com>
2024-08-28 18:00:55 +0800

    refactor: 删除 LC 文档及其部署流程

--- leancloud/conf/env.ts
+++
@@ -1,7 +0,0 @@
-export const BRAND: string = "leancloud";
-export const REGION: string = "cn";
-
-// Cloud Engine
-export const CLI_BINARY: string = BRAND === "tds" ? "tds" : "lean";
-export const HAS_SUB_DOMAIN: boolean = REGION === "global";
-export const HAS_ENGINE_CDN_DOMAIN: boolean = REGION === 'cn'
